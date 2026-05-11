effc67c44d2ecbab93a94ecfee4732ecf1c17746
User <user@example.com>
2026-05-12 07:35:49 +0800

    Simplify railway.json

---
+++ push_railway.ps1
@@ -0,0 +1,3 @@
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" add railway.json
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" commit -m "Update railway.json config"
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" push origin main
