4645e5b650068411b7fbc5ea1da19c6f2945a4c2
User <user@example.com>
2026-05-12 13:15:46 +0800

    Remove Dockerfile, keep only package.json for Nixpacks

---
+++ push_nixpacks.ps1
@@ -0,0 +1,3 @@
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" add -A
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" commit -m "Remove Dockerfile, keep only package.json for Nixpacks"
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" push origin main
---
+++ push_package.ps1
@@ -0,0 +1,3 @@
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" add package.json
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" commit -m "Update root package.json with start script"
+git -C "d:\Backup\Pictures\fluffy-octo-umbrella" push origin main
